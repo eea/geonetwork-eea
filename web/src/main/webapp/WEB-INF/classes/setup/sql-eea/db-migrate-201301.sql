31248d488ad391a150ad4164e627a330e7e85dc2
François Prunayre <fxprunayre@gmail.com>
2013-01-18 09:04:34 +0100

    Add SQL migration script.

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-201301.sql
@@ -0,0 +1,6 @@
+ALTER TABLE HarvestHistory ADD elapsedTime int;
+
+INSERT INTO Settings VALUES (956,1,'hidewithheldelements',NULL);
+INSERT INTO Settings VALUES (957,956,'enable','false');
+INSERT INTO Settings VALUES (958,956,'keepMarkedElement','true');
+INSERT INTO Settings VALUES (24,20,'securePort','8443');
