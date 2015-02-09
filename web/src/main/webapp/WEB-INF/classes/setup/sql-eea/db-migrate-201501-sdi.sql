5235df2215d66154e24016047f34ae1813259e2d
François Prunayre <fxprunayre@gmail.com>
2015-02-09 11:33:35 +0100

    Latest migration options.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-201501-sdi.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-201501-sdi.sql
@@ -7,6 +7,16 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/summaryWidth', '500', 0, 9590, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/mapproj', 'EPSG:3857', 0, 9590, 'n');
 
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/proxy/ignorehostlist', NULL, 0, 560, 'y');
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/atom', 'disabled', 0, 7230, 'y');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/atomSchedule', '0 0 0/24 ? * *', 0, 7240, 'y');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/atomProtocol', 'INSPIRE-ATOM', 0, 7250, 'y');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/metadata/prefergrouplogo', 'y', 0, 9111, 'y');
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('map/isMapViewerEnabled', 'false', 2, 9592, 'n');
 
 UPDATE Settings SET value='3.0.0' WHERE name='version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='subVersion';
+
+
