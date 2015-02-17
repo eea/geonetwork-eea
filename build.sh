742fc65575b066ff12e5f8041f58c7f0a0773fa4
François Prunayre <fxprunayre@gmail.com>
2015-02-17 16:14:33 +0100

    Update build to include map cat.

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-mvn clean install
+mvn clean install -DskipTests
 cd web
-mvn package -Penv-catalogue
-mvn package -Penv-internal-catalogue
-mvn package -Penv-editor-catalogue
+mvn package -DskipTests -Penv-catalogue
+mvn package -DskipTests -Penv-internal-catalogue
+mvn package -DskipTests -Penv-editor-catalogue
+mvn package -DskipTests -Penv-map-catalogue
 
 if [ -n "$1" ] ; then
-    tar cvfz target/eea-catalogues.tar.gz target/catalogue.war target/internal-catalogue.war target/editor-catalogue.war
+    tar cvfz target/eea-catalogues.tar.gz target/catalogue.war target/internal-catalogue.war target/editor-catalogue.war target/map-catalogue.war
     cp target/eea-catalogues.tar.gz $1/.
 fi
