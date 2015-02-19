ab8632367937e0620141efdc2528e3de29839775
François Prunayre <fxprunayre@gmail.com>
2015-02-19 09:00:49 +0100

    Use cookie to preserve session when using the same XmlRequest object.

--- common/src/main/java/org/fao/geonet/utils/GeonetHttpRequestFactory.java
+++ common/src/main/java/org/fao/geonet/utils/GeonetHttpRequestFactory.java
@@ -7,16 +7,16 @@ import org.apache.http.HeaderElement;
 import org.apache.http.HttpClientConnection;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.Credentials;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.config.SocketConfig;
 import org.apache.http.conn.ConnectionRequest;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.conn.routing.HttpRoute;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.impl.client.*;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.protocol.HttpContext;
 import org.springframework.http.HttpHeaders;
@@ -109,7 +109,7 @@ public class GeonetHttpRequestFactory {
         request.setQuery(url.getQuery());
         request.setFragment(url.getRef());
         request.setUserInfo(url.getUserInfo());
-
+        request.setCookieStore(new BasicCookieStore());
         return request;
     }
 
@@ -156,7 +156,7 @@ public class GeonetHttpRequestFactory {
         final HttpClientBuilder clientBuilder = getDefaultHttpClientBuilder();
         configurator.apply(clientBuilder);
         CloseableHttpClient httpClient = clientBuilder.build();
-        if (r.isPreemptiveBasicAuth()) {
+        if (r.isPreemptiveBasicAuth() || r.getHttpClientContext() != null) {
             return new AdaptingResponse(httpClient, httpClient.execute(request, r.getHttpClientContext()));
         } else {
             return new AdaptingResponse(httpClient, httpClient.execute(request));
