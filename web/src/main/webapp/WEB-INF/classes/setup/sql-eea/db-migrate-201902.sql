27cc57bb8ccb2c524cc311d365e5e1532f549025
Francois Prunayre <fx.prunayre@gmail.com>
2019-02-28 15:10:44 +0100

    Region thesaurus / Macedonia is now North Macedonia since February 2019 #3633.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-201902.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-201902.sql
@@ -5,3 +5,4 @@ UPDATE metadata SET data = replace(data, 'former Yugoslavian Republic of Macedon
 UPDATE metadata SET data = replace(data, 'Former Yugoslav Republic of Macedonia', 'North Macedonia') WHERE  data LIKE '%Former Yugoslav Republic of Macedonia%';
 UPDATE metadata SET data = replace(data, 'Republic of Macedonia', 'North Macedonia') WHERE  data LIKE '%Republic of Macedonia%';
 UPDATE metadata SET data = replace(data, '>Macedonia<', '>North Macedonia<') WHERE  data LIKE '%>Macedonia<%';
+UPDATE metadata SET data = replace(data, 'former Yugoslav North Macedonia', 'North Macedonia') WHERE  data LIKE '%former Yugoslav North Macedonia%';
