8ef65913b8d692ca1a59be6a8b1ed81b86b598cf
Francois Prunayre <fx.prunayre@gmail.com>
2019-04-29 07:42:56 +0200

    Remove 19115-3 schema.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v370/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v370/migrate-default.sql
@@ -2,5 +2,16 @@
 INSERT INTO Settings_ui (id, configuration) (SELECT 'srv', value FROM Settings WHERE name = 'ui/config');
 DELETE FROM Settings WHERE name = 'ui/config';
 
+
+-- Remove deprecated (and  unused) ISO19115-3 schema.
+DELETE FROM operationallowed WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115-3'));
+DELETE FROM metadatarating WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115-3'));
+DELETE FROM metadatafiledownloads WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115-3'));
+DELETE FROM metadatafileuploads WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115-3'));
+DELETE FROM metadatanotifications WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115-3'));
+DELETE FROM metadatastatus WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115-3'));
+DELETE FROM validation WHERE metadataid in (SELECT id FROM metadata WHERE schemaid in ('iso19115-3'));
+DELETE FROM metadata WHERE schemaid in ('iso19115-3');
+
 UPDATE Settings SET value='3.7.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
