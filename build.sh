ade337ed6f702375a151bd652724e1d12167fda4
Francois Prunayre <fx.prunayre@gmail.com>
2019-11-04 11:38:36 +0100

    Build / Catalogue build is now internal & public node.

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 mvn clean install -DskipTests
 cd web
 mvn package -DskipTests -Penv-catalogue
-mvn package -DskipTests -Penv-internal-catalogue
 mvn package -DskipTests -Penv-editor-catalogue
-mvn package -DskipTests -Penv-map-catalogue
 
 if [ -n "$1" ] ; then
-    tar cvfz target/eea-catalogues.tar.gz target/catalogue.war target/internal-catalogue.war target/editor-catalogue.war target/map-catalogue.war
+    tar cvfz target/eea-catalogues.tar.gz target/catalogue.war target/editor-catalogue.war
     cp target/eea-catalogues.tar.gz $1/.
 fi
