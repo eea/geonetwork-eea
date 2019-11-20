f93bc59693c45f5c5c7ddd6058cde040393dbcdd
Francois Prunayre <fx.prunayre@gmail.com>
2019-11-20 14:06:34 +0100

    SQL update to reset privileges.

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-201911.sql
@@ -0,0 +1,15 @@
+-- Remove DOWNLOAD operation for group INTERNET to not display SDI dataset links to anonymous users.
+DELETE FROM operationallowed WHERE groupid = 1 AND operationid = 1;
+
+-- Remove all operation for group INTRANET.
+DELETE FROM operationallowed WHERE groupid = 0;
+
+-- Set VIEW/VISUALIZE/DOWNLOAD operation for group INTRANET for all public records
+INSERT INTO operationallowed (groupid, metadataid, operationid)
+  SELECT 0, id, 0 FROM metadata WHERE isTemplate = 'n' AND id IN (SELECT metadataid FROM operationallowed WHERE groupid = 1 AND operationid = 0);
+
+INSERT INTO operationallowed (groupid, metadataid, operationid)
+  SELECT 0, id, 1 FROM metadata WHERE isTemplate = 'n' AND id IN (SELECT metadataid FROM operationallowed WHERE groupid = 1 AND operationid = 0);
+
+INSERT INTO operationallowed (groupid, metadataid, operationid)
+  SELECT 0, id, 5 FROM metadata WHERE isTemplate = 'n' AND id IN (SELECT metadataid FROM operationallowed WHERE groupid = 1 AND operationid = 0);
