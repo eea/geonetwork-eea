283ecaa053353079b27eaa1bb9e4dd5901784ed3
François Prunayre <fx.prunayre@gmail.com>
2019-11-26 10:41:17 +0100

    HTTPS for discomap URL

--- web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-201911.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-201911.sql
@@ -21,3 +21,13 @@ UPDATE metadata SET data = replace(data, 'ISO 19115:2003/19139', 'ISO 19115/1913
 UPDATE metadata SET data = replace(data, 'http://sdi.eea.europa.eu/internal-catalogue', 'https://sdi.eea.europa.eu/catalogue') WHERE data LIKE '%http://sdi.eea.europa.eu/internal-catalogue%';
 
 UPDATE metadata SET data = replace(data, '/internal-catalogue', '/catalogue') WHERE data LIKE '%/internal-catalogue%';
+
+
+UPDATE metadata SET data = replace(data, 'http://bio.discomap.eea.europa.eu', 'https://bio.discomap.eea.europa.eu') WHERE data LIKE '%http://bio.discomap.eea.europa.eu%';
+UPDATE metadata SET data = replace(data, 'http://land.discomap.eea.europa.eu', 'https://land.discomap.eea.europa.eu') WHERE data LIKE '%http://land.discomap.eea.europa.eu%';
+UPDATE metadata SET data = replace(data, 'http://water.discomap.eea.europa.eu', 'https://water.discomap.eea.europa.eu') WHERE data LIKE '%http://water.discomap.eea.europa.eu%';
+UPDATE metadata SET data = replace(data, 'http://discomap.eea.europa.eu', 'https://discomap.eea.europa.eu') WHERE data LIKE '%http://discomap.eea.europa.eu%';
+UPDATE metadata SET data = replace(data, 'http://image.discomap.eea.europa.eu', 'https://image.discomap.eea.europa.eu') WHERE data LIKE '%http://image.discomap.eea.europa.eu%';
+UPDATE metadata SET data = replace(data, 'http://marine.discomap.eea.europa.eu', 'https://marine.discomap.eea.europa.eu') WHERE data LIKE '%http://marine.discomap.eea.europa.eu%';
+UPDATE metadata SET data = replace(data, 'http://test.discomap.eea.europa.eu', 'https://test.discomap.eea.europa.eu') WHERE data LIKE '%http://test.discomap.eea.europa.eu%';
+UPDATE metadata SET data = replace(data, 'http://staging.discomap.eea.europa.eu', 'https://staging.discomap.eea.europa.eu') WHERE data LIKE '%http://staging.discomap.eea.europa.eu%';
