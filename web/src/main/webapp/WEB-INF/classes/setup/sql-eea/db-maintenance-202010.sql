45e1cb2eb15e6c8c44433077c7e7245402283ef9
Francois Prunayre <fx.prunayre@gmail.com>
2020-10-27 12:14:54 +0100

    EEA / SQL / Update usersearch.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-maintenance-202010.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-maintenance-202010.sql
@@ -1,2 +1,13 @@
 UPDATE metadata SET data = replace(data, '>2005 8-9</gml', '>2005-08-09</gml') WHERE data LIKE '%2005 8-9</gm%';
 UPDATE metadata SET data = replace(data, '>2010-0814</gml', '>2010-08-14</gml') WHERE data LIKE '%2010-0814</gml%';
+
+
+
+UPDATE usersearch SET url = 'any=q(-codelist_status:obsolete%20%2BresourceIdentifier:copernicus*)' WHERE url = 'any=*copernicus_*&facet.q=status%2Fnotobsolete&resultType=details&sortBy=relevance';
+UPDATE usersearch SET url = 'any=q(-codelist_status:obsolete%20%2Bany:IDP_*)' WHERE url = 'any=idp_*&facet.q=status%2Fnotobsolete&resultType=details&sortBy=relevance';
+UPDATE usersearch SET url = 'any=q(-codelist_status:obsolete%20%2Bcat:marine)';
+UPDATE usersearch SET url = 'any=q(-codelist_status:obsolete%20%2Bcat:water)' WHERE url = '_cat=water&facet.q=status%2Fnotobsolete&resultType=details&sortBy=relevance';
+UPDATE usersearch SET url = 'any=q(-codelist_status:obsolete%20%2Bcat:biodiversity)' WHERE url = '_cat=biodiversity&facet.q=status%2Fnotobsolete&resultType=details&sortBy=relevance';
+UPDATE usersearch SET url = 'any=q(-codelist_status:obsolete%20%2Bcat:reference)' WHERE url = '_cat=reference&facet.q=status%2Fnotobsolete&resultType=details&sortBy=relevance';
+UPDATE usersearch SET url = 'any=q(+thesaurus_geonetworkthesaurusexternalthemehttpinspireeceuropaeumetadatacodelistPriorityDatasetPriorityDataset:*)' WHERE url = 'any=*PriorityDataset*&resultType=details&sortBy=relevance';
+UPDATE usersearch SET url = 'any=q(-codelist_status:obsolete%20%2Bcat:reference)&sortBy=dateStamp&sortOrder=desc' WHERE url = '_cat=reference&facet.q=status%2Fnotobsolete&resultType=details&sortBy=changeDate';
