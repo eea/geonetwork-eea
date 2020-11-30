5aeb5e7ede7510339b20de9f4a6b7e17142bfbc6
Francois Prunayre <fx.prunayre@gmail.com>
2020-11-30 11:26:47 +0100

    Migration for portal filters.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-202011.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-202011.sql
@@ -2,3 +2,4 @@ CREATE TABLE metadatastatus_202011_backup AS SELECT * FROM metadatastatus;
 
 DROP TABLE metadatastatus;
 
+UPDATE sources SET filter = replace(filter, '-codelist_status', '-cl_status.default') WHERE filter LIKE '%%';
