bffb3be5d58f9c8640b6478f3f86a30fd8b6956d
Francois Prunayre <fx.prunayre@gmail.com>
2021-04-20 13:45:45 +0200

    GN-UI / Add web component lib.

---
+++ web-ui/src/main/resources/catalog/ui/update.sh
@@ -0,0 +1,2 @@
+curl https://geonetwork.github.io/geonetwork-ui/master/demo/webcomponents/gn-wc.js > gn-wc.js
+curl https://geonetwork.github.io/geonetwork-ui/master/demo/webcomponents/gn-results-list.sample.html > list.html
