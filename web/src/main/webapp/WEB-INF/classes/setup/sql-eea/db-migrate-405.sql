802f5562333f9fbdb9ebad1d5f4ce0f39054034f
Francois Prunayre <fx.prunayre@gmail.com>
2021-06-14 10:41:12 +0200

    SQL / Migration / 4.0.5.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-405.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-405.sql
@@ -5,16 +5,21 @@ DELETE FROM Schematroncriteriagroup WHERE schematronid IN (SELECT id FROM schema
 DELETE FROM Schematron WHERE filename LIKE 'schematron-rules-inspire%';
 
 
-UPDATE Settings SET value='4.0.4' WHERE name='system/platform/version';
+UPDATE Settings SET value='4.0.5' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
 
 -- ALTER TABLE Settings ADD COLUMN encrypted VARCHAR(1) DEFAULT 'n';
-UPDATE Settings SET encrypted='y' WHERE name='system/proxy/password';
-UPDATE Settings SET encrypted='y' WHERE name='system/feedback/mailServer/password';
-UPDATE Settings SET encrypted='y' WHERE name='system/publication/doi/doipassword';
+-- UPDATE Settings SET encrypted='y' WHERE name='system/proxy/password';
+-- UPDATE Settings SET encrypted='y' WHERE name='system/feedback/mailServer/password';
+-- UPDATE Settings SET encrypted='y' WHERE name='system/publication/doi/doipassword';
 
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/security/passwordEnforcement/minLength', '6', 1, 12000, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/security/passwordEnforcement/maxLength', '20', 1, 12001, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/security/passwordEnforcement/usePattern', 'true', 2, 12002, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/security/passwordEnforcement/pattern', '^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).*)$', 0, 12003, 'n');
+
+
+
+alter table operationallowed
+    drop constraint operationallowed_metadataid_fkey;
