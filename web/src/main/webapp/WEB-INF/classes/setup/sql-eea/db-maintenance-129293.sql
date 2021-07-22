3281f51931b17e3fa55a21702844837beff578eb
Francois Prunayre <fx.prunayre@gmail.com>
2021-07-22 14:05:57 +0200

    EEA / Add five days. https://taskman.eionet.europa.eu/issues/129293#change-604160.

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-maintenance-129293.sql
@@ -0,0 +1,4 @@
+-- https://taskman.eionet.europa.eu/issues/129293#change-604160
+
+UPDATE metadata SET data = replace(data, '>EU28<', '>EU28 (2013-2020)<') WHERE data LIKE '%>EU28<%';
+UPDATE metadata SET data = replace(data, '>EU27<', '>EU27 (2007-2013)<') WHERE data LIKE '%>EU27<%';
