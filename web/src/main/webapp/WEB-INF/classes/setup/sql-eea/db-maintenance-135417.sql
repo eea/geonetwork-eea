a06c96783f1091b10a6a6823b89a187b8c38a0fe
Francois Prunayre <fx.prunayre@gmail.com>
2021-07-23 08:23:25 +0200

    Kosovo update https://taskman.eionet.europa.eu/issues/135417.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-maintenance-135417.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-maintenance-135417.sql
@@ -1,7 +1,7 @@
 UPDATE metadata
     SET data = REPLACE(data,
                        '>Kosovo<',
-                       '> (UNSCR 1244/99)<')
+                       '>Kosovo (UNSCR 1244/99)<')
     WHERE data LIKE '%>Kosovo<%';
 
 UPDATE metadata
