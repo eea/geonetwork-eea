377fc974aff6fcff657bb936ef7b42296c966233
Francois Prunayre <fx.prunayre@gmail.com>
2021-07-23 07:36:46 +0200

    Kosovo update https://taskman.eionet.europa.eu/issues/135417.

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-maintenance-135417.sql
@@ -0,0 +1,23 @@
+UPDATE metadata
+    SET data = REPLACE(data,
+                       '>Kosovo<',
+                       '> (UNSCR 1244/99)<')
+    WHERE data LIKE '%>Kosovo<%';
+
+UPDATE metadata
+    SET data = REPLACE(data,
+                       'Kosovo and Serbia',
+                       'Kosovo (UNSCR 1244/99) and Serbia')
+    WHERE data LIKE '%Kosovo and Serbia%';
+
+UPDATE metadata
+    SET data = REPLACE(data,
+                       '>Kosovo under the UN Security Council Resolution 1244/99<',
+                       '>Kosovo (UNSCR 1244/99)<')
+    WHERE data LIKE '%>Kosovo under the UN Security Council Resolution 1244/99<%';
+
+UPDATE metadata
+    SET data = REPLACE(data,
+                       'Kosovo under UNSC Resolution 1244/99',
+                       'Kosovo (UNSCR 1244/99)')
+    WHERE data LIKE '%Kosovo under UNSC Resolution 1244/99%';
