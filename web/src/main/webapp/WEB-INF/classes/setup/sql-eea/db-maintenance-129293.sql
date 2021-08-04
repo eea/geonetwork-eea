e7192e41f93f67cdaa4837f1ef98d96ea496c885
François Prunayre <fx.prunayre@gmail.com>
2021-08-04 07:34:35 +0200

    Update db-maintenance-129293.sql

--- web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-maintenance-129293.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-maintenance-129293.sql
@@ -2,3 +2,4 @@
 
 UPDATE metadata SET data = replace(data, '>EU28<', '>EU28 (2013-2020)<') WHERE data LIKE '%>EU28<%';
 UPDATE metadata SET data = replace(data, '>EU27<', '>EU27 (2007-2013)<') WHERE data LIKE '%>EU27<%';
+UPDATE metadata SET data = replace(data, '>EU27_2007<', '>EU27 (2007-2013)<') WHERE data LIKE '%>EU27_2007<%';
