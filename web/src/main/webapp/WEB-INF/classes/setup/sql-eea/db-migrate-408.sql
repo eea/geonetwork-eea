1170820692eceb855cb19cdfc82a5e5edecea736
Francois Prunayre <fx.prunayre@gmail.com>
2021-10-04 15:08:15 +0200

    EEA / 4.0.8 update.

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-408.sql
@@ -0,0 +1,9 @@
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/remotevalidation/nodeid', '', 0, 7212, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/inspire/remotevalidation/apikey', '', 0, 7213, 'y');
+
+INSERT INTO Users (id, username, password, name, surname, profile, kind, organisation, security, authtype, isenabled) VALUES  (0,'nobody','','nobody','nobody',4,'','','','', 'n');
+INSERT INTO Address (id, address, city, country, state, zip) VALUES  (0, '', '', '', '', '');
+INSERT INTO UserAddress (userid, addressid) VALUES  (0, 0);
+
+UPDATE Settings SET value='4.0.8' WHERE name='system/platform/version';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
