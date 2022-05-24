43c0e83371a10ddaa43beb43d39e20d880d84035
Francois Prunayre <fx.prunayre@gmail.com>
2022-05-24 14:15:05 +0200

    EEA / SQL update / Fix settings.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-420.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-420.sql
@@ -27,6 +27,7 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 UPDATE Settings SET editable = 'n' WHERE name = 'system/userFeedback/lastNotificationDate';
 UPDATE Settings SET editable = 'n' WHERE name = 'system/security/passwordEnforcement/pattern';
 
+UPDATE Settings SET internal = 'n' WHERE name = 'system/metadata/prefergrouplogo';
 
 UPDATE Settings SET value='4.2.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
