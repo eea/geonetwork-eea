e9b6b022cbacf366cb091def937266211e54ba02
michimau <mauro.michielon@eea.europa.eu>
2022-08-22 14:39:46 +0200

    rename of the image name

--- build-in-docker/build_last_image.sh
+++ build-in-docker/build_last_image.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-
-
-
-
-git ls-remote https://github.com/eea/geonetwork-eea.git HEAD | cut -f1
-
-
-docker build -t michimau/gn:$(git ls-remote https://github.com/eea/geonetwork-eea.git HEAD | cut -f1) .
+docker build -t eeacms/geonetwork-eea:$(git ls-remote https://github.com/eea/geonetwork-eea.git HEAD | cut -f1) .
