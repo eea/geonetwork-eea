b08732f2433c232e34830fc2c38704c2d1752c0a
Francois Prunayre <fx.prunayre@gmail.com>
2023-12-18 15:12:47 +0100

    EEA / 4.4.2.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-440.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-440.sql
@@ -1,8 +1,8 @@
 alter table files
 alter column content type oid using content::oid;
-  
+
 alter table settings
 alter column internal char default 'y'
 
-UPDATE Settings SET value='4.4.0' WHERE name='system/platform/version';
+UPDATE Settings SET value='4.4.2' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
