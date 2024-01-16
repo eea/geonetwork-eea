abe17301d865c31af57c2a342eeaa59b1ec7752c
Juan Luis Rodríguez Ponce <juanluisrp@gmail.com>
2024-01-16 13:59:44 +0100

    Update db-migrate-440.sql
    Add migrations from GN 4.4.0, 4.4.1 and 4.4.2.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-440.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-440.sql
@@ -1,8 +1,17 @@
-alter table files
-alter column content type oid using content::oid;
+ALTER TABLE files
+ALTER COLUMN content type oid USING content::oid;
 
-alter table settings
-alter column internal char default 'y'
+ALTER TABLE settings
+ALTER COLUMN internal char DEFAULT 'y'
 
+-- From 4.4.0
+INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'metadata/batchediting/accesslevel', 'Editor', 0, 12020, 'n' FROM settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'metadata/batchediting/accesslevel');
+
+-- From 4.4.1
+INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'metadata/pdfReport/headerLogoFileName', '', 0, 12508, 'y' FROM settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'metadata/pdfReport/headerLogoFileName');
+
+-- From 4.4.2
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/useGeodesicExtents', 'false', 2, 9591, 'n');
+  
 UPDATE Settings SET value='4.4.2' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
