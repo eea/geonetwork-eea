f1c28165d3e5e754f03c5367587e5cfc7e2db2c1
Juan Luis Rodríguez Ponce <juanluisrp@gmail.com>
2024-01-16 14:12:45 +0100

    Update db-migrate-440.sql
    Fix syntax

--- web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-440.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-eea/db-migrate-440.sql
@@ -2,10 +2,10 @@ ALTER TABLE files
 ALTER COLUMN content type oid USING content::oid;
 
 ALTER TABLE settings
-ALTER COLUMN internal char DEFAULT 'y'
+ALTER COLUMN internal  type char;
 
--- From 4.4.0
-INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'metadata/batchediting/accesslevel', 'Editor', 0, 12020, 'n' FROM settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'metadata/batchediting/accesslevel');
+ALTER TABLE settings
+ALTER COLUMN internal SET  DEFAULT 'y';
 
 -- From 4.4.1
 INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'metadata/pdfReport/headerLogoFileName', '', 0, 12508, 'y' FROM settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'metadata/pdfReport/headerLogoFileName');
