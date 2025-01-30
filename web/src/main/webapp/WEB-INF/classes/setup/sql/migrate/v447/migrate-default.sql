6c72fe0c81151f812fccff58ea22715f5aa3cf30
Jose García <josegar74@gmail.com>
2025-01-30 14:01:21 +0100

    Setting to configure whether editor users who are metadata owners can edit their metadata when they do not have editing privileges for the metadata.

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v447/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v447/migrate-default.sql
@@ -2,3 +2,6 @@ UPDATE Settings SET value='4.4.7' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/banner/enable', 'false', 2, 1920, 'n');
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/auditable/enable', 'false', 2, 12010, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/metadataprivs/userAlwaysCanEditOwnedMetadata', 'false', 2, 9184, 'n');
