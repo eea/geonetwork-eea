e54c9b9dee81a94284f5866c6e2eae33b5174a9d
Francois Prunayre <fx.prunayre@gmail.com>
2025-02-12 19:10:05 +0100

    EEA / Admin / Link analysis / Add EEA shortcut action https://taskman.eionet.europa.eu/issues/281127.

--- web-ui/src/main/resources/catalog/js/admin/DashboardRecordLinkController.js
+++ web-ui/src/main/resources/catalog/js/admin/DashboardRecordLinkController.js
@@ -176,6 +176,30 @@
           }
         }
       });
+      
+      $scope.eeaRecordSearch = function() {
+        $http
+          .post(
+            "../api/search/records/_search?bucket=eea",
+            {
+              size: 0,
+              query: {
+                query_string: {
+                  query: '-cl_status.key:(obsolete or superseded) +isTemplate:n +isHarvested:false'
+                }
+              }
+            }
+          )
+          .then(function (r) {
+            $http.put("../api/selections/eea").then(function (r) {
+              $scope.selectedSelection.id = "eea";
+              $scope.urlFilter = "https://sdi.eea.europa.eu/data/";
+              $http.get("../api/selections/eea").then(function (r) {
+                $scope.triggerSearch();
+              });
+            });
+          });
+      }
 
       $element.on("post-body.bs.table", function () {
         $element.find("a[data-job-key]").click(function (event) {
