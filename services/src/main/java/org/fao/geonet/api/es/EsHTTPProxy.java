28319275f16da013dd1c2b3511967e9b79131386
Francois Prunayre <fx.prunayre@gmail.com>
2025-07-21 13:27:29 +0200

    Merge fix.

--- services/src/main/java/org/fao/geonet/api/es/EsHTTPProxy.java
+++ services/src/main/java/org/fao/geonet/api/es/EsHTTPProxy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001-2025 Food and Agriculture Organization of the
+ * Copyright (C) 2018-2025 Food and Agriculture Organization of the
  * United Nations (FAO-UN), United Nations World Food Programme (WFP)
  * and United Nations Environment Programme (UNEP)
  *
@@ -235,14 +235,6 @@ public class EsHTTPProxy {
                     if (opFields != null) {
                         for (JsonNode field : opFields) {
                             final int groupId = field.asInt();
-<<<<<<< HEAD
-=======
-                            if (operation == ReservedOperation.editing
-                                && !canEdit
-                                && editingGroups.contains(groupId)) {
-                                canEdit = true;
-                            }
->>>>>>> origin/44-scheduledpublication
 
                             if (groups.contains(groupId)) {
                                 operations.add(operation);
@@ -461,11 +453,7 @@ public class EsHTTPProxy {
                         }
                     }
                 }
-<<<<<<< HEAD
-                requestBody.append(jsonNode.toString()).append(System.lineSeparator());
-=======
-                requestBody.append(node).append(System.lineSeparator());
->>>>>>> origin/44-scheduledpublication
+                requestBody.append(jsonNode).append(System.lineSeparator());
             }
             handleRequest(context, httpSession, request, response, url, endPoint,
                 requestBody.toString(), true, selectionBucket, relatedTypes);
@@ -706,20 +694,11 @@ public class EsHTTPProxy {
                 if (doc.has("_source")) {
                     ObjectNode sourceNode = (ObjectNode) doc.get("_source");
 
-                    if (sourceNode.has(Geonet.IndexFieldNames.SCHEMA)) {
-                        String metadataSchema = sourceNode.get(Geonet.IndexFieldNames.SCHEMA).asText();
-                        try {
-                            MetadataSchema mds = schemaManager.getSchema(metadataSchema);
+                    String metadataSchema = doc.get("_source").get("documentStandard").asText();
+                    MetadataSchema mds = schemaManager.getSchema(metadataSchema);
 
-                            // Apply metadata schema filters to remove non-allowed fields
-                            processMetadataSchemaFilters(context, mds, doc);
-                        } catch (IllegalArgumentException e) {
-                            LOGGER.error("Failed to load metadata schema for {}. Error is: {}",
-                                getSourceString(doc, Geonet.IndexFieldNames.UUID),
-                                e.getMessage()
-                            );
-                        }
-                    }
+                    // Apply metadata schema filters to remove non-allowed fields
+                    processMetadataSchemaFilters(context, mds, doc);
 
                     // Remove fields with privileges info
                     for (ReservedOperation o : ReservedOperation.values()) {
