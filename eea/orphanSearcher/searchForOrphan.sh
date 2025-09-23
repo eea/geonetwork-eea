5bf2d0f6eb66ee75a331b1ec3de1806c4b3009d2
Francois Prunayre <fx.prunayre@gmail.com>
2025-09-23 22:31:01 +0200

    EEA / Update orphan searcher script.

--- eea/orphanSearcher/searchForOrphan.sh
+++ eea/orphanSearcher/searchForOrphan.sh
@@ -6,16 +6,30 @@ type=dataset
 from=0
 size=1000
 
+QUERY="+isHarvested:false -cl_status.key:(superseded OR obsolete) +resourceType: $type"
+
+read -r -d '' ESQUERY << EOF
+  {
+    "from":${from},
+    "size":${size},
+    "query":{"query_string":{"query":"${QUERY//\"/\\\"}"}},
+    "sort": [{"resourceTitleObject.default.keyword": "asc"}],
+    "_source":{"includes":["uuid", "resourceTitleObject*", "resourceIdentifier"]}
+  }
+EOF
+
+  RAWQUERY=`echo ${ESQUERY}`
+
 rm results.json
 rm orphan$type.csv
 
 curl $AUTH "$SERVER/srv/api/search/records/_search?relatedType=parent" \
     -X 'POST' \
-    -H 'Accept: application/json, text/plain, */*' \
+    -H 'Accept: application/json' \
     -H 'Content-Type: application/json;charset=UTF-8' \
-    --data-raw "{\"query\":{\"query_string\":{\"query\": \"+isHarvested:false -cl_status.key:(superseded OR obsolete) +resourceType: $type\"}},\"from\":$from, \"size\":$size, \"_source\": {\"include\": [\"resourceTitleObject.default\", \"resourceIdentifier.code\"]}, \"sort\": [{\"resourceTitleObject.default.keyword\": \"asc\"}]}" \
-    --compressed \
+    --data-raw "$RAWQUERY"  \
     -o results.json
+
 
 for hit in $(jq -r '.hits.hits[] | @base64' results.json); do
    _jq() {
