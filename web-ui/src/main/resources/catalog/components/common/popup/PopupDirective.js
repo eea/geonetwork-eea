a9f54ac2e7a4ba8ffe548ca27f7be2bff12cabd1
Francois Prunayre <fx.prunayre@gmail.com>
2025-09-28 14:30:51 +0200

    Metadata delete dialog
    Co-authored-by: Jose García <josegar74@gmail.com>

--- web-ui/src/main/resources/catalog/components/common/popup/PopupDirective.js
+++ web-ui/src/main/resources/catalog/components/common/popup/PopupDirective.js
@@ -45,12 +45,40 @@
             title: ""
           };
         }
+
+        // Checks if requires a confirm action word to enable the confirm button
+        scope.hasConfirmActionWord = angular.isDefined(scope.options.confirmActionWord);
+
+        // User input for confirm action word
+        scope.userInput = "";
+
         element.addClass("gn-popup modal fade");
 
         // handle close callback
         if (scope.options.closeCallback) {
-          element.on("hidden.bs.modal", scope.options.closeCallback);
+          element.on("hidden.bs.modal", function () {
+            scope.userInput = "";
+            scope.options.closeCallback();
+          });
+        } else {
+          element.on("hidden.bs.modal", function () {
+            scope.userInput = "";
+          });
         }
+
+        /**
+         * Checks to disable the confirm button if a confirm action word
+         * is defined and the user input does not match it.
+         *
+         * @returns {boolean}
+         */
+        scope.disableConfirmButton = function () {
+          if (scope.hasConfirmActionWord && scope.options.confirmActionWord !== "") {
+            return scope.userInput !== scope.options.confirmActionWord;
+          }
+
+          return false;
+        };
       }
     };
   });
