baefad2f0bc3d21ddd87512b6f254bcd7061f1a4
Francois Prunayre <fx.prunayre@gmail.com>
2026-09-11 08:04:29 +0200

    Indexing / Fix field type while changing sharing.
    When using sharing API, EsSearchManager.updateFields was applying toArray conversion to all fields.
    This could generate isPublishedToAll: [true] instead of expected isPublishedToAll: true.
    It was not causing issue in standard GN but was the cause of ClassCastException in some custom events.
    
    Properly convert fields depending on types:
    * "op*" fields are array
    * arrayFields are too
    * others are value

--- core/src/test/java/org/fao/geonet/kernel/search/EsSearchManagerTest.java
+++ core/src/test/java/org/fao/geonet/kernel/search/EsSearchManagerTest.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.jdom.Element;
@@ -62,5 +65,19 @@ public class EsSearchManagerTest {
         JsonNode expected = objectMapper.readTree("{\"field1\":\"content1\",\"indexingErrorMsg\":[{\"type\":\"error\",\"string\":\"string-reference\",\"values\":{\"value1\":\"content1\",\"value2\":\"content2\"}}]}");
 
         assertEquals(expected, result);
+    }
+
+    @Test
+    public void toFieldMapPreservesScalarBooleanAndKeepsOpArrays() {
+        Multimap<String, Object> fields = ArrayListMultimap.create();
+        fields.put("isPublishedToAll", true);
+        fields.put("op1", 1);
+        fields.put("op1", 2);
+
+        Object result = instance.toFieldMap(fields).get("isPublishedToAll");
+        Object[] opValues = (Object[]) instance.toFieldMap(fields).get("op1");
+
+        assertEquals(Boolean.TRUE, result);
+        assertArrayEquals(new Object[]{1, 2}, opValues);
     }
 }
